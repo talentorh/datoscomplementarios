f3f00c6f935421a90d497004ad464ff9b5016e15
Talento RH <talentorh@hraei.gob.mx>
2023-12-12 14:27:14 -0600

    carga de datos

--- frontend/login.php
+++ frontend/login.php
@@ -57,12 +57,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     
 include("claseConexion/conexion.php");
 
-    $statement = $conexionSeleccion->prepare('SELECT correoelectronico, rfc FROM datospersonales WHERE correoelectronico= :correoelectronico AND rfc = :rfc and acceder = :acceder');
+    $statement = $conexionSeleccion->prepare('SELECT correoelectronico, rfc FROM datospersonales WHERE correoelectronico= :correoelectronico AND rfc = :rfc');
     $statement->execute(array(
         
         ':correoelectronico' => $correo,
         ':rfc'=>$password,
-        ':acceder'=>3
+
     ));
 
     $resultado = $statement->fetch();
